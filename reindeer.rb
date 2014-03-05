1faed9149c4d52229a2c1b84eb40ec5512060ed4
steve <stevedanko1@gmail.com>
2014-03-04 20:53:14 -0500

    refactored

--- reindeer.rb
+++ reindeer.rb
@@ -1,9 +1,4 @@
 def reindeers(presents)
-  if presents > 180
-    raise 'Santa can only take a max of 180 presents'
-  elsif presents <= 30 && presents > 0
-    3
-  else
-    (presents.to_f/30.0).ceil + 2
-  end
+  raise 'Santa can only take a max of 180 presents' if presents > 180
+  (presents.to_f/30.0).ceil + 2
 end
